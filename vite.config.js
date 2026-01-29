74aef538c6e642743bd8fce3d5977bc408c103b6
rara <rrara1605@gmail.com>
2026-01-29 08:40:58 +0700

    firs commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: '/',  // Set the base path for the application
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+  base: "/",
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://www.freetogame.com",
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
