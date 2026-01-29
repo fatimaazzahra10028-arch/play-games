f7ce5ba89e7364676d5a180ab12f68f8a366aef2
rara <rrara1605@gmail.com>
2026-01-29 07:20:52 +0700

    firs commit

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -85,7 +85,6 @@ function Favorites() {
         </div>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-          // Render favorite games
           {state.favorites.map((game) => (
             <GameCard key={game.id} game={game} /> // GameCard component
           ))}
